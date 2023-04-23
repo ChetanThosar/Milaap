cf28625b75c7c3f6f267e4c177b75e7d3d096941
ChetanThosar <48657396+ChetanThosar@users.noreply.github.com>
2023-04-24 01:43:08 +0530

    passing parameters in link

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{items_id}/")
+def get_data(items_id:int,q: Union[str,None]=None):
+    return {"items_id":items_id,"q":q}
+
